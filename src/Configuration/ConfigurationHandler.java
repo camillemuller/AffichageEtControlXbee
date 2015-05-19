ad0fad13c23304fe225e9ed730d45506fe69dc87
Muller Camille <camille.muller@etu.univ-tours.fr>
2015-05-19 14:42:03 +0200

    Menage

--- src/Configuration/ConfigurationHandler.java
+++ src/Configuration/ConfigurationHandler.java
@@ -2,13 +2,11 @@ package Configuration;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
-import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
